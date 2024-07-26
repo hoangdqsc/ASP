ca58d39e624fd81ab5559b9aa6118b824610921a
hoangdqsc <106561683+hoangdqsc@users.noreply.github.com>
2024-07-26 09:49:53 +0700

    Create BookDto.cs

---
+++ BookDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Book.DTOs
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
