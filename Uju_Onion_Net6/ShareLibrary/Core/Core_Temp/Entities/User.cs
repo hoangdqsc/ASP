20074825afcb1946748313f0ef82fa6e5bcd34da
hoangdqsc <106561683+hoangdqsc@users.noreply.github.com>
2024-08-06 11:37:02 +0700

    Create User.cs

---
+++ Uju_Onion_Net6/ShareLibrary/Core/Core_Temp/Entities/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Temp.Entites
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; } // Xác nhận rằng thuộc tính Email tồn tại
+        public string PasswordHash { get; set; }
+    }
+}
