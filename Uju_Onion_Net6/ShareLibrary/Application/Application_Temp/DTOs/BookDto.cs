16a362e529a0c1a69b5d1480f8a7dee0f35a1e20
hoangdqsc <106561683+hoangdqsc@users.noreply.github.com>
2024-08-08 14:43:30 +0700

    Create BookDto.cs

---
+++ Uju_Onion_Net6/ShareLibrary/Application/Application_Temp/DTOs/BookDto.cs
@@ -0,0 +1,9 @@
+namespace Application_Temp.DTOs
+{
+    public class BookDto
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+}
