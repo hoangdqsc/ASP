73b4a6cf7e060699ebe91ee9b0aca48ac7c9896d
hoangdqsc <106561683+hoangdqsc@users.noreply.github.com>
2024-08-08 14:32:55 +0700

    Create Book.cs

---
+++ Uju_Onion_Net6/ShareLibrary/Core/Core_Temp/Entities/Book.cs
@@ -0,0 +1,10 @@
+namespace Core_Temp.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+}
