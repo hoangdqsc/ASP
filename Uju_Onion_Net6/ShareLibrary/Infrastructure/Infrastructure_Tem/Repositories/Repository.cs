9995fe9208517fc40779654bf9d15a57f3263981
hoangdqsc <106561683+hoangdqsc@users.noreply.github.com>
2024-08-09 09:18:28 +0700

    Create Repository.cs

---
+++ Uju_Onion_Net6/ShareLibrary/Infrastructure/Infrastructure_Tem/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+using Core_Temp.Interfaces;
+using Infrastructure_Temp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure_Temp.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
