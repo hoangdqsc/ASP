38434e4ab190eae173b3a593da6106a7890dfbd7
hoangdqsc <106561683+hoangdqsc@users.noreply.github.com>
2024-08-20 08:03:50 +0700

    Add files via upload

---
+++ Uju_Onion_Net6/ShareLibrary/Core/Core_Plc/Interfaces/IPlcRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core_Plc.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Plc.Interfaces
+{
+    public interface IPlcRepository
+    {
+       
+            Task<PLCData> ReadCoilsAsync(string ipAddress, int port, ushort startAddress, ushort numberOfPoints);
+            Task<PLCError> GetPlcErrorAsync(string ipAddress, int port);
+            Task<DeviceInfo> GetDeviceInfoAsync(string ipAddress, int port);
+        
+    }
+}
---
+++ Uju_Onion_Net6/ShareLibrary/Core/Core_Plc/Interfaces/IPlcService.cs
@@ -0,0 +1,19 @@
+﻿using Core_Plc.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Core_Plc.Interfaces
+{
+          public interface IPlcService
+        {
+            Task<PLCData> GetPlcDataAsync(string ipAddress, int port, ushort startAddress, ushort numberOfPoints);
+            Task<PLCError> GetPlcErrorAsync(string ipAddress, int port);
+            Task<DeviceInfo> GetDeviceInfoAsync(string ipAddress, int port);
+        }
+
+   
+}
